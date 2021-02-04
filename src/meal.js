694e669dc0e0cda700b98323d8869ce80ddb078b
Nichele Dunn <the7yearitch@hotmail.com>
2021-02-04 15:26:19 -0700

    Add nameMenuItem and createMenuItem functions and update statements

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,27 @@
-function nameMenuItem() {
+function nameMenuItem(name) {
+  return `Delicious ${name}`;
+};
 
-}
+function createMenuItem(name, price, type) {
+  var menuItem = {
+    name: name,
+    price: price,
+    type: type,
+  };
 
+  console.log(menuItem.name);
+  return menuItem;
+  // error - cannot read property 'name' of undefined (failing on line 44 of test)
+  // the test is requiring an output on line 44 of menuItem.name, 'Delicious French Toast'
+  // the console.log is returning Delicious French Toast
+  // Had to add a `return` statement - works fine now!
+};
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
+  createMenuItem,
   // addIngredients,
   // formatPrice,
   // decreasePrice,
   // createRecipe
-}
-
-
+};
