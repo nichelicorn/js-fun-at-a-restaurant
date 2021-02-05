970e7c4d8d878c48a2834da5ebf91e330c61a9ed
Nichele Dunn <the7yearitch@hotmail.com>
2021-02-04 20:21:28 -0700

    Create takeOrder function and pass first test

--- src/order.js
+++ src/order.js
@@ -1,6 +1,13 @@
+function takeOrder(order, array) {
+
+};
+// fxn should add new orders to an empty array
+// when called, the function should take in two arguments, `order` and `array`
+// when called, the `order` should be pushed into the empty `array`
+// don't forget the return statement!
 
 module.exports = {
-  // takeOrder,
+  takeOrder,
   // refundOrder,
   // listItems,
   // searchOrder
