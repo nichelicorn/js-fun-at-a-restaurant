61e1c599661d407249e0b1971e427a022f214c6e
Nichele Dunn <the7yearitch@hotmail.com>
2021-02-05 14:01:03 -0700

    Create refundOrder function and pass first test

--- src/order.js
+++ src/order.js
@@ -14,9 +14,16 @@ function takeOrder(order, array) {
 // array should not be able to hold more than three Objects √
 // if array is = 3, do nothing; else push the new order to the array √
 
+function refundOrder() {
+
+};
+// : expected 3 to equal 2; this is expecting that once deliveryOrders has 3 orders, the first order should be removed? I think this because when the function is called on line 154 of the test, it includes a number that corresponds to the first order. Next, on line 157, the `assert.deepEqual` statement is returing the array of deliveryOrders, and it is displaying a value of just var order2 && order3
+// When the function is invoked, 2 arguments are passed through (will want to set up two parameters)
+
+
 module.exports = {
   takeOrder,
-  // refundOrder,
+  refundOrder,
   // listItems,
   // searchOrder
 }
