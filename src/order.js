69cd057142e152b5f2752726a8baa7631d0512a6
Nichele Dunn <the7yearitch@hotmail.com>
2021-02-06 17:43:01 -0700

    Complete searchOrder function and Iteration 2!!

--- src/order.js
+++ src/order.js
@@ -1,9 +1,7 @@
 function takeOrder(order, array) {
   if (array.length >= 3) {
-    // console.log(array);
     return array;
   } else {
-    // console.log(array);
     array.push(order);
   }
 };
@@ -12,24 +10,25 @@ function refundOrder(num, array) {
   array.splice(num - 1, 1)
 };
 
-
 function listItems(array) {
   var orderList = [];
   for (var i = 0; i < array.length; i++) {
     orderList.push(array[i].item);
   }
-  // console.log('List of orders:', orderList.join(', '));
+
   return orderList.join(', ');
 };
 
-function searchOrder(array) {
+function searchOrder(array, item) {
+  var orderList = [];
   for (var i = 0; i < array.length; i++) {
-    
-    if (array.includes(array[i].item)) {
-      return true;
-    } else {
-      return false;
-    }
+    orderList.push(array[i].item);
+  };
+
+  if (orderList.includes(item)) {
+    return true;
+  } else {
+    return false;
   }
 };
 
