1697ce3013a17aaa81bffcaa38981ddc35d99f57
Nichele Dunn <the7yearitch@hotmail.com>
2021-02-07 17:23:49 -0700

    Pass some Chef tests

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,16 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+    this.name = name;
+    this.restaurant = restaurant;
   }
-}
+
+  greetCustomer(cust, bool) {
+    if (bool === true) {
+      return `Good morning, ${cust}!`;
+    } else {
+      return `Hello, ${cust}!`;
+    }
+  }
+};
 
 module.exports = Chef;
